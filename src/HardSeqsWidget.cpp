e4a790df528a728a0fb610232584a4dd4acfffdf
John Titor <foolycoolyfly@zoho.com>
2025-04-30 04:08:28 +0300

    add: license GPL-3.0

--- src/HardSeqsWidget.cpp
+++ src/HardSeqsWidget.cpp
@@ -1,3 +1,22 @@
+/*
+ * This file is part of HardSeqs.
+ *
+ * HardSeqs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HardSeqs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HardSeqs. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2025 regular-dev team
+ */
+
 #include "HardSeqs.hpp"
 
 #include "UiComponents.hpp"
@@ -6,8 +25,6 @@
 #include <chrono>
 #include <cstdint>
 
-// RACK_GRID_WIDTH = 15
-// RACK_GRID_HEIGHT = 380
 
 struct HardSeqsWidget : ModuleWidget 
 {
--- src/RandomGenerator.hpp
+++ src/RandomGenerator.hpp
@@ -1,3 +1,22 @@
+/*
+ * This file is part of HardSeqs.
+ *
+ * HardSeqs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HardSeqs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HardSeqs. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2025 regular-dev team
+ */
+
 #pragma once
 
 #include <iostream>
